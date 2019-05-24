88a29462edfdf5bea7fa3274ad3c64a01194e2ee
Antonis Gkortzis <antonis.gkortzis@gmail.com>
2019-05-24 17:59:27 +0300

    Fix minor issues in histogram generator module

--- histogramgenerator/src/main/java/histogramgenerator/JFreeChartXYLineChartDemo.java
+++ histogramgenerator/src/main/java/histogramgenerator/JFreeChartXYLineChartDemo.java
@@ -11,6 +11,7 @@ import org.jfree.data.xy.XYSeriesCollection;
  * 
  * @author agkortzis The purpose of this class is to demonstrate a simple
  *         scenario of a JFreeChart XYLine chart.
+ * @since May 2019 
  */
 public class JFreeChartXYLineChartDemo {
 
